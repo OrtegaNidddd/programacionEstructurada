e832ffe7108c934c568791b9d8c8e27b21ab1b21
Nick Ortega <orteganid@gmail.com>
2025-04-07 14:59:00 -0500

    Arreglos en los ejemplos de 28 marzo

--- Ejemplos 28 Marzo/Ejemplo 1/ejemplo1-28-Marzo.cpp
+++ Ejemplos 28 Marzo/Ejemplo 1/ejemplo1-28-Marzo.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    
-    for (int i=50;i<=100;i+=2){
-            cout << i << endl;
+    // Ciclo for, se inicializa i con el valor de 50 y el ciclo funciona hasta que i sea menor o igual a 100
+    // Se aumenta de a 2 i hasta llegar al 100
+    for (int i=49;i<=100;i+=2|){
+        cout << i << endl;
+        // Imprime i
     }
 
     return 0;
+    // Programa finaliza con exito
 }
--- Ejemplos 28 Marzo/Ejemplo 3/ejemplo3-28-Marzo.cpp
+++ Ejemplos 28 Marzo/Ejemplo 3/ejemplo3-28-Marzo.cpp
@@ -2,12 +2,23 @@
 using namespace std;
 
 int main() {
+    //Inicializamos la variable numerosMultiplos con un valor de 0
     int numerosMultiplos = 0;
 
+    //Ciclo For se declara i en 0 y se aumenta i en 1 hasta que sea menor o igual a 10000
     for(int i = 0 ; i <= 10000; i++) {
+
+    //Se evalua si en la posicion de i en el momento es multiplo de 13 con operador modulo
         if (i % 13 == 0){
-            numerosMultiplos++;  
+            // Se imprime i que es el numeor multiplo de 13
+            cout << i << endl;
+            // Si i es multiplo de 13 se suma 1 a la variable numerosMultiplos
+
+            numerosMultiplos++;
         }
     }
+    //Imprime la variable numerosMultiplos
     cout << numerosMultiplos << endl;
+
+    return 0;
 }
